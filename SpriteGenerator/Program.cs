b095c0216b83de0003bb7e5a31375062fa27353a
tringuyen1032 <trinmse140981@fpt.edu.vn>
2022-02-08 19:57:31 +0700

    release

--- SpriteGenerator/Program.cs
+++ SpriteGenerator/Program.cs
@@ -11,11 +11,12 @@ namespace SpriteGenerator
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SpritesForm());
+            if (args.Length == 1 && args[0] == "INSTALLER") { System.Diagnostics.Process.Start(Application.ExecutablePath); return; }
         }
     }
 }
